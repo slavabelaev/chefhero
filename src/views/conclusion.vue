18ddf049cc5768d604a7e5429b79a2adafcb2b8e
belaevslava <belaevslava@gmail.com>
2018-11-13 13:46:32 +0200

    add styles for hr

--- src/views/conclusion.vue
+++ src/views/conclusion.vue
@@ -2,9 +2,9 @@
 <article id="conclusion">
     <h1>Conclusion</h1>
     <hr>
-    <p class="lead">All in all, running a restaurant takes a lot of preparation, organization, knowledge, know-how, hard work and  so much more.</p>
-    <p class="lead">From the ideation stage to actually running a restaurant, there is a lot to consider and learn.</p>
-    <p class="lead">The purpose of this guide is to give you the knowledge and tools that you’ll need to succeed as a restaurateur. We have covered topics ranging from hiring amazing employees and actually keeping them to what it takes to build a memorable brand for your restaurant.</p>
-    <p class="lead">If you’re just getting into the industry, we hope that this guide has painted an accurate picture for what’s to come and help you better prepare for this new exciting journey you’re about to embark on. If you’re an industry veteran, we hope that this guide has been able to provide you with a new view on how to tackle the many challenges that restaurateurs face on the daily.</p>
+    <p>All in all, running a restaurant takes a lot of preparation, organization, knowledge, know-how, hard work and  so much more.</p>
+    <p>From the ideation stage to actually running a restaurant, there is a lot to consider and learn.</p>
+    <p>The purpose of this guide is to give you the knowledge and tools that you’ll need to succeed as a restaurateur. We have covered topics ranging from hiring amazing employees and actually keeping them to what it takes to build a memorable brand for your restaurant.</p>
+    <p>If you’re just getting into the industry, we hope that this guide has painted an accurate picture for what’s to come and help you better prepare for this new exciting journey you’re about to embark on. If you’re an industry veteran, we hope that this guide has been able to provide you with a new view on how to tackle the many challenges that restaurateurs face on the daily.</p>
 </article>
 </template>
