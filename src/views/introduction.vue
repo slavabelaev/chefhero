51afba6cb76a09f005108f89f9670c9afba2ebd5
belaevslava <belaevslava@gmail.com>
2018-11-14 12:00:39 +0200

    fix styles for article

--- src/views/introduction.vue
+++ src/views/introduction.vue
@@ -2,12 +2,12 @@
 <article id="introduction">
     <header>
         <h1>The Ultimate Guide to Running a Restaurant</h1>
-        <p class="lead">This ultimate guide will provide you with the tools and knowledge you need to succeed as a restaurateur, from<br> pre-launch to actually running your establishment.</p>
+        <p class="lead">This ultimate guide will provide you with the tools and knowledge you need to succeed as a restaurateur, from pre-launch to actually running your establishment.</p>
     </header>
     <section>
         <h1>Introduction</h1>
         <hr>
-        <p>From the outside looking in, owning and running a restaurant seems like a fun time.How hard can it be, right? Wrong</p>
+        <p class="lead">From the outside looking in, owning and running a restaurant seems like a fun time.How hard can it be, right? Wrong</p>
         <p>Nearly 60 percent of new restaurants will fail within their first 3 years. 80 percent will close within their first 5<sup><a target="_blank" href="https://news.osu.edu/restaurant-failure-rate-much-lower-than-commonly-assumed-study-finds/">[1]</a></sup>.</p>
         <p>So, when it comes to the ones that make it past the first 5 years, what are they doing right and what steps can you take to ensure that you’re on the path to success?</p>
         <p>This ultimate guide will provide you with the tools and knowledge you need to succeed as a restaurateur, from  pre-launch to actually running your establishment.</p>
