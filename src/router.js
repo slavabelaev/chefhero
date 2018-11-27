cf11762a79a1094293e0f347e7b22a8a0ab2e9eb
belaevslava <belaevslava@gmail.com>
2018-11-27 13:53:38 +0200

    Change the text in the table of contents to “Start”

--- src/router.js
+++ src/router.js
@@ -27,7 +27,7 @@ export default new Router({
       path: '/',
       name: 'title',
       meta: {
-        title: 'Title',
+        title: 'Start',
         description: '',
         keywords: '',
         needToShowInNavigation: true
